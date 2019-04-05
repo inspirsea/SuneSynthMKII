950bcf2fb591c97cd3152cd8e712920b6860cd74
inspirsea <sunesson.emil@gmail.com>
2019-04-05 16:51:24 +0200

    Initial stuffs

--- src/app/service/synth-master.ts
+++ src/app/service/synth-master.ts
@@ -5,25 +5,46 @@ export class SynthMaster {
 
   private oscillator;
   private ampEnvelope;
+  private gainNode;
 
-  initialize() {
-    this.oscillator = new Tone.Oscillator({
-			"type" : "square",
-			"frequency" : 440,
-			"volume" : 1
-    });
-    
-    let gainNode = Tone.context.createGain();
-    
-    this.ampEnvelope = new Tone.Envelope({
+  private playing = false;
+
+  constructor() {
+    this.initialize();
+  }
+
+  public play() {
+    this.oscillator.start();
+  }
+
+  public key() {
+
+    this.oscillator.frequency.value = 50;
+
+    if(this.playing) {
+      this.playing = false;
+      this.ampEnvelope.triggerRelease();
+    } else {
+      this.playing = true;
+      this.ampEnvelope.triggerAttack();
+    }
+  }
+
+  private initialize() {
+    this.oscillator = new Tone.Oscillator(440, "sine");
+
+    this.ampEnvelope = new Tone.AmplitudeEnvelope({
       "attack" : 0.1,
       "decay" : 0.2,
-      "sustain" : 1,
+      "sustain" : 0.5,
       "release" : 0.8,
     });
 
-    this.ampEnvelope.connect(gainNode.gain);
-
+    this.oscillator.connect(this.ampEnvelope);
+    
+    this.ampEnvelope.toMaster();
   }
+
+  
 
 }
